be773c9b204352744c4113c87ae75989df1cdbcf
Stanislaw Blicharski <mainly.agn@gmail.com>
2024-12-01 18:14:23 +0100

    Fix font configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: '../../public/fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: '../../public/fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
